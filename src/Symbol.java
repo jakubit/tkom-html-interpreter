19f5af151f8d0ec76249b686d1564cef8346a835
jakub <pawlak011@gmail.com>
2018-12-01 19:22:46 +0100

    Added enum for Symbols

---
+++ src/Symbol.java
@@ -0,0 +1,13 @@
+public enum Symbol {
+    attrributeAssing,
+    attributeName,
+    attributeValue,
+    beginStarttag,
+    beginEndTag,
+    beginComment,
+    tagName,
+    finishTag,
+    finishComment,
+    data,
+    beginDoctype;
+}
