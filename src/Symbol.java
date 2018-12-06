f3a16c15a3aaed6388b52b568df61ba6d80576b0
jakub <pawlak011@gmail.com>
2018-12-06 13:30:57 +0100

    added symbols

--- src/Symbol.java
+++ src/Symbol.java
@@ -1,17 +1,47 @@
-public enum Symbol {
-    attrributeAssing,
-    attributeName,
-    attributeValue,
-    beginStartTag,
-    beginEndTag,
-    beginComment,
-    tagName,
-    finishTag,
-    finishComment,
-    data,
-    beginDoctype,
-    other,
-    singleQuote,
-    doubleQuote,
-    EOI;
+
+public class Symbol {
+
+    public enum SymbolType {
+        attrributeAssing,
+        attributeName,
+        attributeValue,
+        beginStartTag,
+        beginEndTag,
+        beginComment,
+        tagName,
+        finishTag,
+        finishComment,
+        data,
+        beginDoctype,
+        other,
+        singleQuote,
+        doubleQuote,
+        alphabetic,
+        EOI;
+    }
+
+    private SymbolType type;
+    private String value;
+
+    public Symbol(SymbolType type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public SymbolType getType() {
+        return type;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return type + "\t" + value;
+    }
+
 }
+
+
+
