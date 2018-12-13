9da765b120147697771de263ac3645fcb316d467
jakub <pawlak011@gmail.com>
2018-12-13 11:37:16 +0100

    Main refactor

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,17 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!\n  ");
 
-
-        /*try (FileReader reader = new FileReader("/Users/JakubPawlak/OneDrive/STUDNIA/Semestr 6/TKOM/Projekt/Etap 1/index.html")) {
-            do {
-                char c = (char) reader.read();
-                System.out.println(c);
-            } while (System.in.read() != 0 );
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-        Source s = new Source("/Users/JakubPawlak/IdeaProjects/TKOM/src/index.html");
+        Source s = new Source("src/config.html");
         Lexer l = new Lexer(s);
 
         s.open();
 
         Symbol symbol;
 
-            do {
-                symbol = l.nextSymbol();
-                System.out.println(symbol);
-            } while (symbol.getType() != Symbol.SymbolType.EOF);
-
-
-
-
-
+        do {
+            symbol = l.nextSymbol();
+            System.out.println(symbol);
+        } while (symbol.getType() != Symbol.SymbolType.EOF);
     }
 }
