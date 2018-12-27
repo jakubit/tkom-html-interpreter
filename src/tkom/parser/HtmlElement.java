ac96437b0a15fe3b037878ff79da7e7eb095fbdd
jakub <pawlak011@gmail.com>
2018-12-27 14:12:19 +0100

    Html Content: toString improved, content added

--- src/tkom/parser/HtmlElement.java
+++ src/tkom/parser/HtmlElement.java
@@ -9,9 +9,32 @@ public class HtmlElement {
     }
 
     private final ElementType type;
+    private String content;
 
     public HtmlElement(ElementType type) {
         this.type = type;
+    }
+
+    public HtmlElement(ElementType type, String content) {
+        this.type = type;
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        String toReturn = "Type: " + type;
+        if (type != ElementType.tag) {
+            toReturn += "\tContent: " + content;
+        }
+        return toReturn;
     }
 
 }
