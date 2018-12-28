40e9cf6185593d27d3f5e54289f6dbc3b3195211
jakub <pawlak011@gmail.com>
2018-12-28 13:00:53 +0100

    EOF Exception added

---
+++ src/tkom/parser/UnexpectedEOFException.java
@@ -0,0 +1,24 @@
+package tkom.parser;
+
+import tkom.source.TextPosition;
+
+public class UnexpectedEOFException extends Exception {
+    private String expected;
+    private TextPosition position;
+
+    public UnexpectedEOFException(String expected, TextPosition position) {
+        this.expected = expected;
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        string.append("UNEXPECTED END OF FILE! ");
+        string.append(position);
+        string.append(" Expected: ");
+        string.append(expected);
+
+        return string.toString();
+    }
+}
